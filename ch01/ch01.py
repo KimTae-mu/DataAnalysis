37f5697d41c522601cf7d4c10d3ae1f8dc66934f
kimtae <1160846772@qq.com>
2018-02-05 14:44:30 +0800

    数据分析，ch01

--- ch01/ch01.py
+++ ch01/ch01.py
@@ -3,7 +3,7 @@ import os
 homedir = os.getcwd()
 
 path = 'usagov_bitly_data2012-03-16-1331923249.txt'
-'''大解放发生的奇偶发爱妃我免费电视'''
+''''''
 records = [json.loads(line)
            for line in open(path)]
 print(records[0])
