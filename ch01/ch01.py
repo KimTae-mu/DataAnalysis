9f220a6f29fa6983a01940c573ad03c699e3ec46
kimtae <1160846772@qq.com>
2018-02-16 17:01:47 +0800

    数据分析，ch01

--- ch01/ch01.py
+++ ch01/ch01.py
@@ -2,8 +2,7 @@ import json
 import os
 homedir = os.getcwd()
 
-path = 'usagov_bitly_data2012-03-16-1331923249.txt'
-''''''
+path = 'lib/usagov_bitly_data2012-03-16-1331923249.txt'
 records = [json.loads(line)
            for line in open(path)]
 print(records[0])
